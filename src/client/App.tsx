45651383026325f3a4153d353ad47759ead764eb
Marcelo Mottalli <marcelo@grandata.com>
2018-12-25 03:35:49 -0300

    Adding client

---
+++ src/client/App.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App: React.SFC = () => <div>This is a React component</div>;
+
+export default App;
---
+++ src/client/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/client/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../../_output/static")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html")
+    })
+  ]
+};
